57028679691c2cf02814455bafbb4b5bb4df1094
Klára Madiová <madiovak@gmail.com>
2022-02-12 21:42:23 +0100

    Vypracovani

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+document.querySelector("#vysledek").innerHTML = secti(7, 3);
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
+ * 
+ */
+function zobrazVysledek(selector, a, b) {
+  document.querySelector(selector).innerHTML = secti(a, b);
+}
+/*
  * @param {int} a
  * @param {int} b
  */
@@ -13,9 +19,25 @@ function secti(a, b) {
   return c;
 }
 
+
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log('Gratulace, právě si spustila tuto funkci!');
+
+
+  let squareElement = document.getElementsByClassName('ctverecek')[0]
+  squareElement.innerHTML = 'Gratulace, právě si spustila tuto funkci!';
+  squareElement.style.width = '350px';
+  squareElement.style.backgroundColor = 'pink';
+
+}
+
+
+
+function zmenaBarvy() {
+  document.querySelector('.ctverecek').style.backgroundColor = 'green';
 }
